7779424aa3578bc80202ef0b4bf0bed114e46d8d
Kaisei <me@kaisei.dev>
2024-12-04 22:49:03 +0900

    day4

--- calendar.js
+++ calendar.js
@@ -1,6 +1,8 @@
 const surprises = [
   { title: "LLM Agents can Autonomously Hack Websites", link: "https://kaisei.dev/posts/LLM_Agents_can_Autonomously_Hack_Websites.md" },
   { title: "An Attacker’s Dream? Exploring the Capabilities of ChatGPT for Developing Malware", link: "https://kaisei.dev/posts/An_Attacker's_Dream_Exploring_the_Capabilities_of_ChatGPT_for_Developing_Malware.md" },
+  { title: "Missed!", link: "#" },
+  { title: "When LLMs Meet Cybersecurity: A Systematic Literature Review", link: "https://kaisei.dev/posts/When_LLMs_Meet_Cybersecurity_A_Systematic_Literature_Review.md" },
 ];
 
 const calendar = document.getElementById("calendar");
