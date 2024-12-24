d5f42b1c7944ce7b7d61c63a23061c40a8494eae
Kaisei <me@kaisei.dev>
2024-12-24 17:16:47 +0900

    day24

--- calendar.js
+++ calendar.js
@@ -6,6 +6,7 @@ const surprises = [
   { title: "eyeballvul: a future-proof benchmark for vulnerability detection in the wild", link: "https://kaisei.dev/posts/eyeballvul_a_future-proof_benchmark_for_vulnerability_detection_in_the_wild.md" },
   { title: "Hacking, The Lazy Way: LLM Augmented Pentesting", link: "https://kaisei.dev/posts/Hacking_The_Lazy_Way_LLM_Augmented_Pentesting.md" },
   { title: "Fuzz4All: Universal Fuzzing with Large Language Models", link: "https://kaisei.dev/posts/Fuzz4All_Universal_Fuzzing_with_Large_Language_Models.md" },
+  { title: "Is Generative AI the Next Tactical Cyber Weapon For Threat Actors? Unforeseen Implications of AI Generated Cyber Attack", link: "https://kaisei.dev/posts/Is_Generative_AI_the_Next_Tactical_Cyber_Weapon_For_Threat_Actors_Unforeseen_Implications_of_AI_Generated_Cyber_Attacks.md" },
   { title: "The Anonymity of the Dark Web: A Survey 01", link: "https://kaisei.dev/posts/The_Anonymity_of_the_Dark_Web_A_Survey_01.md" },
   { title: "The Anonymity of the Dark Web: A Survey 02", link: "https://kaisei.dev/posts/The_Anonymity_of_the_Dark_Web_A_Survey_02.md" },
   { title: "The Anonymity of the Dark Web: A Survey 03", link: "https://kaisei.dev/posts/The_Anonymity_of_the_Dark_Web_A_Survey_03.md" },
@@ -20,6 +21,8 @@ const surprises = [
   { title: "Cross-Site Scripting Attacks and DefensiveTechniques: A Comprehensive Survey", link: "https://kaisei.dev/posts/Cross_Site_Scripting_Attacks_and_Defensive_Techniques_A_Comprehensive_Survey.md" },
   { title: "Conti Inc. : Understanding the Internal Discussions of a large Ransomware-as-a-Service Operator with Machine Learning", link: "https://kaisei.dev/posts/Conti_Inc_Understanding_the_Internal_Discussions_of_a_large_Ransomware_as_a_Service_Operator_with_Machine_Learning.md" },
   { title: "Cyber Threat Intelligence Model: An Evaluation of Taxonomies, Sharing Standards, and Ontologies within Cyber Threat Intelligence", link: "https://kaisei.dev/posts/Cyber_Threat_Intelligence_Model_An_Evaluation_of_Taxonomies_Sharing_Standards_and_Ontologies_within_Cyber_Threat_Intelligence.md" },
+  { title: "Missed!", link: "#" },
+  { title: "A Holistic Approach to Ransomware Classification: Leveraging Static and Dynamic Analysis with Visualizatio", link: "https://kaisei.dev/posts/A_Holistic_Approach_to_Ransomware_Classification_Leveraging_Static_and_Dynamic_Analysis_with_Visualization.md" },
 ];
 
 
